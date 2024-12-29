31db7325cf1eee6a886f63643c80d65c9013d142
Jaskirat174 <jaskirat@sikhsparks.org>
2024-12-29 07:04:50 -0500

    create issue model

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,32 @@
+"use client"
 import Link from 'next/link'
 import React from 'react' 
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 
 const NavBar = () => {
 
+  const path = usePathname()
+  console.log(path)
+
   const items = [
-    {label:"dashboard",href:"/"},
-    {label:"Issues", href:"/issues"}]
+    {label:"Dashboard",href:"/"},
+    {label:"Issues", href:"/issues"}
+  ]
+
   return (
     <nav className='flex space-x-6 pl-5 border-b items-center h-14 mb-5'>
       <Link href="/"><FaBug/></Link>
       <ul className='flex space-x-6'>
       {items.map(item=> (
-        <li>
-          <Link href={item.href} className='text-zinc-400 hover:text-black transition-colors'>
+        <li key={item.href}>
+          <Link href={item.href} className={classNames({
+            'text-black':item.href===path,
+            'text-zinc-300':item.href!==path,
+            'hover:text-black transition-colors':true
+          })}>
             {item.label}
           </Link>
         </li>
@@ -23,5 +35,7 @@ const NavBar = () => {
     </nav>
   )
 }
+
+// `${item.href===path? 'text-black':'text-zinc-400'} text-zinc-400 hover:text-black transition-colors`}
 
 export default NavBar
