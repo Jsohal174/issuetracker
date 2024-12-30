b3b7dd7843edb687b254d10a6ec5e0320c29c473
Jaskirat174 <jaskirat@sikhsparks.org>
2024-12-30 04:46:31 -0500

    add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,9 @@
+import BadgeComp from '@/app/components/BadgeComp'
 import prisma from '@/prisma/client'
+import { Card, Heading, Text } from '@radix-ui/themes'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+
 
 
 
@@ -13,12 +17,19 @@ const page = async ({ params }: { params: { id: string }}) => {
 
   return (
     <div>
-      <p>{issue!.id}</p>
-      <p>{issue!.description}</p>
-      <p>{issue!.status}</p>
-      <p>{issue!.createdAt.toDateString()}</p>
-      <p>{issue!.updatedAt.toDateString()}</p>
+      <Heading>{issue!.id}</Heading>
 
+      <div className='flex items-center space-x-3 my-3'>
+        <BadgeComp status={issue!.status}/>
+        <Text>{issue!.createdAt.toDateString()}</Text>
+      </div>
+     
+      <Card className='prose mt-3'>
+        <ReactMarkdown>
+          {issue!.description}
+        </ReactMarkdown>
+      </Card>
+      
     </div>
   )
 }
