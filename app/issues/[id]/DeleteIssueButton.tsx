11600b3c4acec9e5d8876a5fbee04eb479cd9120
Jaskirat174 <jaskirat@sikhsparks.org>
2025-01-02 07:41:19 -0500

    Show confirmation dialog button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
 import { MdEdit } from 'react-icons/md'
@@ -6,12 +7,30 @@ import { MdEdit } from 'react-icons/md'
 
 const DeleteIssueButton = ({issueId}: {issueId: number}) => {
   return (
-    <Button color='red'>
-      <MdEdit/>
-      <Link href={`/issues/${issueId}/delete`}>
-        Delete Issue
-      </Link>   
-    </Button>
+
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+            Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>
+          Confirm Deletion
+        </AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue ?
+        </AlertDialog.Description>
+        <Flex className='mt-4 gap-3'>
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'> Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'> Delete Issue </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
